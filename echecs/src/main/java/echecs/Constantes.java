e56c9ec77a64be6b9e0869de1e5afa97a43e9d69
Huguesmmm <hugues.prudhomme123@gmail.com>
2021-03-09 21:42:28 -0500

    module 3 : ajout de fichier json

--- echecs/src/main/java/echecs/Constantes.java
+++ echecs/src/main/java/echecs/Constantes.java
@@ -1,6 +1,6 @@
 package echecs;
 
 public class Constantes {
-	public static final String[] IDS_MODELES_TESTS = {"test01"};
+	public static final String[] IDS_MODELES_TESTS = {"test01","test02","test03"};
 	public static final int DIMENSION_PLATEAU = 8;
 }
