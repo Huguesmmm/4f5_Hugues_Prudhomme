9eca722edef2f7fdd5a5d3afc8e0b4775375be55
Huguesmmm <hugues.prudhomme123@gmail.com>
2021-03-12 11:35:44 -0500

    module4 : création de la vue VuePartie CRÉATION: ficher structure.xml CRÉATION: vue VuePartie FAIT: création du tableau de cases à afficher

--- echecs/src/main/java/echecs/Constantes.java
+++ echecs/src/main/java/echecs/Constantes.java
@@ -3,4 +3,6 @@ package echecs;
 public class Constantes {
 	public static final String[] IDS_MODELES_TESTS = {"test01","test02","test03"};
 	public static final int DIMENSION_PLATEAU = 8;
+
+	public static final int TAILLE_CASE = 50;
 }
--- echecs/src/main/java/echecs/pages/partie/vues/VuePartie.java
+++ echecs/src/main/java/echecs/pages/partie/vues/VuePartie.java
@@ -1,4 +1,77 @@
 package echecs.pages.partie.vues;
 
-public class VuePartie {
+import echecs.Constantes;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import ntro.debogage.DoitEtre;
+import ntro.debogage.J;
+import ntro.mvc.Vue;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VuePartie implements Vue, Initializable {
+    @FXML
+    private VBox conteneurPlateau;
+
+    private Button[][] cases;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        J.appel(this);
+        DoitEtre.nonNul(conteneurPlateau);
+    }
+    public void creerPlateau(){
+        J.appel(this);
+        creerColonnes();
+    }
+
+    public void creerColonnes(){
+        J.appel(this);
+
+        for(int indiceRangee = 0; indiceRangee < Constantes.DIMENSION_PLATEAU; indiceRangee++){
+            HBox rangee = creerRangee(indiceRangee);
+            conteneurPlateau.getChildren().add(rangee);
+        }
+    }
+    public HBox creerRangee(int indiceRangee){
+        J.appel(this);
+        HBox rangee = new HBox();
+        for(int indiceColonne = 0; indiceColonne < Constantes.DIMENSION_PLATEAU; indiceColonne++){
+            Button aCase = creerCase();
+            cases[indiceColonne][indiceColonne] = aCase;
+            rangee.getChildren().add(aCase);
+        }
+        return rangee;
+    }
+    public Button creerCase(){
+        J.appel(this);
+        Button aCase = new Button();
+
+        aCase.setMinSize(Constantes.TAILLE_CASE, Constantes.TAILLE_CASE);
+        aCase.setMaxSize(Constantes.TAILLE_CASE, Constantes.TAILLE_CASE);
+
+        return aCase;
+    }
+
+
+
+
+    @Override
+    public void obtenirCommandesPourEnvoi() {
+        J.appel(this);
+    }
+
+    @Override
+    public void installerCapteursEvenementsUsager() {
+        J.appel(this);
+    }
+
+    @Override
+    public void verifierCommandesPossibles() {
+        J.appel(this);
+    }
 }
