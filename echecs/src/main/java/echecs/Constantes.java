9b281ad3d51dbe018a1067c09f2cdcba84b59755
Huguesmmm <hugues.prudhomme123@gmail.com>
2021-03-31 17:30:39 -0400

    constantes et pagepartielocale modifié FAIT: ajout de constantes

--- echecs/src/main/java/echecs/Constantes.java
+++ echecs/src/main/java/echecs/Constantes.java
@@ -1,19 +1,54 @@
 package echecs;
 
 public class Constantes {
-
-	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/partie/locale/structure.xml";
-	public static final String CHEMIN_IMAGES = "/resources/images/";
-
-	public static final String[] IDS_MODELES_TESTS = {"test01","test02","test03","complet"};
+	// ------ CONSTANTES DU PROGRAMME ------
+	// partie
 	public static final int DIMENSION_PLATEAU = 8;
 
+	// ------ FICHIER JSON ------
+	// partie
+	public static final String[] IDS_MODELES_TESTS = {"test01","test02","test03","complet"};
+	// leaderboard
+	public static final String[] JSON_MODELES_TESTS = {"modeleJson1","modeleJson2","modeleJson3"};
+
+	// ------ VUE XML ------
+	// accueil
+	public static final String CHEMIN_ACCUEIL_FXML = "/accueil/structure.xml";
+	// partie
+	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/partie/locale/structure.xml";
+	// leaderboard
+	public static final String CHEMIN_LEADERBOARD_FXML = "/leaderboard/structure.xml";
+
+	// ----- VUE TAILLE -----
+	// partie
 	public static final int TAILLE_CASE = 80;
+	public static final int SIZE_PARTIE_PIXELS = 740;
+	// leaderboard
+	public static final int LARGEUR_PARAMETRES_PIXELS = 300;
+	public static final int HAUTEUR_PARAMETRES_PIXELS = 520;
 
-	public static final int SIZE_PIXELS = 740;
+	public static final int LARGEUR_PIXELS = 600;
+	public static final int HAUTEUR_PIXELS= 800;
 
+	public static final int LARGEUR_PARAMETRES_PIXELS_MIN = 250;
+	public static final int HAUTEUR_PARAMETRES_PIXELS_MIN = 500;
+
+	public static final int LARGEUR_PARAMETRES_PIXELS_MAX = 350;
+	public static final int HAUTEUR_PARAMETRES_PIXELS_MAX = 540;
+
+	public static final int LARGEUR_PIXELS_MIN = 400;
+	public static final int HAUTEUR_PIXELS_MIN = 600;
+
+	// ------ VUE STYLE CSS ------
+	// partie
 	public static final String STYLE_BLACK_OCCUPIED = "-fx-font-size:30; -fx-background-color: #4b7399;";
 	public static final String STYLE_WHITE_OCCUPIED = "-fx-font-size:30; -fx-background-color: #eae9d2;";
 	public static final String STYLE_WHITE_UNOCCUPIED = "-fx-background-color: #eae9d2";
 	public static final String STYLE_BLACK_UNOCCUPIED = "-fx-background-color: #4b7399";
+
+
+	// ------ RESSOURCES ------
+	// partie - images
+	public static final String CHEMIN_IMAGES = "/resources/images/";
+
 }
